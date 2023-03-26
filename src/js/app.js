295d350622ad4618d468cec2319acc3c097b77c4
Tryd0g0lik <work80@mail.ru>
2023-03-26 13:19:31 +0700

    Testing  appveyor without bable

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,24 @@
-// TODO: write your code here
-import sum from './basic';
+const hero = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
 
-console.log('worked');
+try {
+  if (!!hero && hero.length > 0) {
+    hero.sort((a, b) => {
+      if (a.health > b.health) {
+        return 1;
+      }
+      if (a.health < b.health) {
+        return -1;
+      }
 
-console.log(sum([1, 2]));
+      return 0;
+    });
+
+    console.log(hero);
+  }
+} catch (e) {
+  console.log(e.message);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -6,6 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
+
   },
   module: {
     rules: [
