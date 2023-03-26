cc56dd086e978ddfe8d87623f2bafdc399179f04
Tryd0g0lik <work80@mail.ru>
2023-03-26 18:24:24 +0700

    will need settings the test/ Maybe will be needed delete a 'test.each(['

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,26 +1,23 @@
 /* eslint-disable import/extensions */
 import { hero, cortedHeroes } from '../app.js';
 
-
-const heroList = test.each([
-
-  ['healthy A01', { name: 'мечник', health: 10 }, 'wounded'],
-  ['healthy B01', { name: 'маг', health: 100 }, 'healthy'],
-  ['healthy C01', { name: 'лучник', health: 80 }, 'healthy'],
-]);
-
-heroList('test the healtly statuses wich has been equal ==> %s ', (a, b, expected) => {
-  // The hero's list
+describe('the La Croix cans on my desk', () => {
+  // heroList('test the healtly statuses wich has been equal ==> %s ', (a, b, expected) => {
+  //   // The hero's list
   const can1 = cortedHeroes(hero); // The Heroes has been sorted by the health
+  const heroList = test.each([
 
-  describe('the La Croix cans on my desk', () => {
-    test('have all the same properties', () => {
-      expect(can1).toEqual(expected);
-    });
-
-    test('are not the exact same can', () => {
-      expect(can1).not.toBe(expected);
-    });
+    ['healthy A01', can1[2], { name: 'мечник', health: 10 }],
+    ['healthy B01', can1[0], { name: 'маг', health: 100 }],
+    ['healthy C01', can1[1], { name: 'лучник', health: 80 }],
+  ]);
+  heroList('have all the same properties', () => {
+    for (const elem in can1) {
+      if (!elem === false) {
+        expect(elem).toEqual(heroList);
+      }
+    }
   });
 });
+// });
 
