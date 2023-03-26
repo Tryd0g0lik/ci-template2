79bb30e6a7f686bd4f7bd890ef4964951bad63c3
Tryd0g0lik <work80@mail.ru>
2023-03-26 17:08:13 +0700

    Oops! Something went wrong! :(

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,28 @@
-const hero = [
+export const hero = [
   { name: 'мечник', health: 10 },
   { name: 'маг', health: 100 },
   { name: 'лучник', health: 80 },
 ];
 
-try {
-  if (!!hero && hero.length > 0) {
-    hero.sort((a, b) => {
-      if (a.health > b.health) {
-        return 1;
-      }
-      if (a.health < b.health) {
-        return -1;
-      }
+// eslint-disable-next-line consistent-return
+export function cortedHeroes(h) {
+  try {
+    if (!!h.length > 0) {
+      hero.sort((a, b) => {
+        if (a.health > b.health) {
+          return -1;
+        }
+        if (a.health < b.health) {
+          return 1;
+        }
 
-      return 0;
-    });
+        return 0;
+      });
 
-    console.log(hero);
+      return h;
+    }
+    return h;
+  } catch (e) {
+    console.log(e.message);
   }
-} catch (e) {
-  console.log(e.message);
 }
