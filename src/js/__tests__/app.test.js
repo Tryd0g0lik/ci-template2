61912708f22dbe4dc5fe022b5a5da4446a4b7b9c
Tryd0g0lik <work80@mail.ru>
2023-03-27 07:11:37 +0700

    makeing a test working from the console

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -2,22 +2,14 @@
 import { hero, cortedHeroes } from '../app.js';
 
 describe('the La Croix cans on my desk', () => {
-  // heroList('test the healtly statuses wich has been equal ==> %s ', (a, b, expected) => {
-  //   // The hero's list
   const can1 = cortedHeroes(hero); // The Heroes has been sorted by the health
   const heroList = test.each([
-
-    ['healthy A01', can1[2], { name: 'мечник', health: 10 }],
-    ['healthy B01', can1[0], { name: 'маг', health: 100 }],
-    ['healthy C01', can1[1], { name: 'лучник', health: 80 }],
+    [can1[2], { name: 'мечник', health: 10 }],
+    [can1[0], { name: 'маг', health: 100 }],
+    [can1[1], { name: 'лучник', health: 80 }],
   ]);
-  heroList('have all the same properties', () => {
-    for (const elem in can1) {
-      if (!elem === false) {
-        expect(elem).toEqual(heroList);
-      }
-    }
+
+  heroList('have all the same properties name ', (b, expected) => {
+    expect(b).toEqual(expected);
   });
 });
-// });
-
